1b3f396a89527d4815ba7937096cabecf8377f78
Shalopay1337 <andrey.jablonski@rosberry.com>
2021-09-06 17:07:33 +0600

    make layout

--- app/src/main/java/com/example/pine/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/pine/ui/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.pine.R
 
@@ -14,5 +15,7 @@ class SearchFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val buttonBack = view.findViewById<ImageView>(R.id.iv_back)
+        buttonBack.setOnClickListener(View.OnClickListener {  })
     }
 }
